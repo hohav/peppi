fdeee892798704506f41529287b21de06c25f6b1
hohav <hohav@fastmail.com>
2020-08-13 15:59:45 -0700

    provide `try_from` to convert string into pseudo-enum

--- src/pseudo_enum.rs
+++ src/pseudo_enum.rs
@@ -1,3 +1,17 @@
+#[derive(std::fmt::Debug)]
+pub struct ConversionError {
+	pub r#type: String,
+	pub value: String,
+}
+
+impl std::fmt::Display for ConversionError {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "no such {}: {}", self.r#type, self.value)
+	}
+}
+
+impl std::error::Error for ConversionError { }
+
 macro_rules! pseudo_enum {
 	($name:ident : $type:ty { $( $value:expr => $variant:ident ),* $(,)? }) => {
 		#[derive(Copy, Clone, PartialEq, Eq, Hash)]
@@ -5,6 +19,19 @@ macro_rules! pseudo_enum {
 
 		impl $name {
 			$( pub const $variant:$name = $name($value); )*
+		}
+
+		impl std::convert::TryFrom<&str> for $name {
+			type Error = super::pseudo_enum::ConversionError;
+			fn try_from(s: &str) -> std::result::Result<Self, Self::Error> {
+				match s {
+					$( stringify!($variant) => Ok($name::$variant), )*
+					_ => Err(Self::Error {
+						r#type: format!("{}::{}", module_path!(), stringify!($name)),
+						value: s.to_string(),
+					}),
+				}
+			}
 		}
 
 		impl std::fmt::Debug for $name {
