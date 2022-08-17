450aa75e1aea0477bfc16fc8c590aa168ab437c0
Nick Condron <ncondron42@gmail.com>
2022-08-17 14:21:45 -0400

    Separate out regex portion of macro

--- peppi/src/info/character.rs
+++ peppi/src/info/character.rs
@@ -9,13 +9,31 @@ pub struct Info {
 	pub traction: f32,
 }
 
-macro_rules! char_info {
-	($info: ident {
+macro_rules! info {
+	($type: ident => $info: ident {
 		$($name: ident {
-			regex: $regex: expr,
-			$( $field: ident : $value: expr ),* $(,)?
-		});* $(;)?
+			$( $field: ident : $value: expr ),+ $(,)?
+		});+ $(;)?
 	}) => {
+		impl $info {
+			pub fn try_from(external: $type) -> Option<$info> {
+				match external {
+					$( $type::$name => Some($name), )*
+					_ => None
+				}
+			}
+		}
+
+		$(pub const $name: $info = $info {
+			$( $field: $value ),*
+		};)*
+	}
+}
+
+macro_rules! info_regex {
+	($info: ident {
+        $($name: ident : $regex: expr),+ $(,)?
+    }) => {
 		mod regexes {
 			use lazy_static::lazy_static;
 			use regex::Regex;
@@ -25,13 +43,6 @@ macro_rules! char_info {
 		}
 
 		impl $info {
-			pub fn try_from(external: External) -> Option<$info> {
-				match external {
-					$( External::$name => Some($name), )*
-					_ => None
-				}
-			}
-
 			pub fn try_match(s: &str) -> Option<$info> {
 				match s {
 					$( s if regexes::$name.is_match(s) => Some($name), )*
@@ -39,244 +50,274 @@ macro_rules! char_info {
 				}
 			}
 		}
-
-		$(pub const $name: $info = $info {
-			external: External::$name,
-			$( $field: $value ),*
-		};)*
 	}
 }
 
-
-char_info!(Info {
+info!(External => Info {
 	CAPTAIN_FALCON {
-		regex: r"(?i-u)^(capt(ain|\.)?[ _]?)?falcon$",
+        external: External::CAPTAIN_FALCON,
 		short: "Falcon",
 		long: "Captain Falcon",
 		traction: 0.08,
 	};
 
 	DONKEY_KONG {
-		regex: r"(?i-u)^dk|(donkey[ _]?kong$",
+        external: External::DONKEY_KONG,
 		short: "DK",
 		long: "Donkey Kong",
 		traction: 0.08,
 	};
 
 	FOX {
-		regex: r"(?i-u)^fox$",
+        external: External::FOX,
 		short: "Fox",
 		long: "Fox",
 		traction: 0.08,
 	};
 
 	GAME_AND_WATCH {
-		regex: r"(?i-u)^(g|game)[ _]?(and|&|n)[ _]?(w|watch)$",
+        external: External::GAME_AND_WATCH,
 		short: "G&W",
 		long: "Game and Watch",
 		traction: 0.06,
 	};
 
 	KIRBY {
-		regex: r"(?i-u)^kirby$",
+        external: External::KIRBY,
 		short: "Kirby",
 		long: "Kirby",
 		traction: 0.08,
 	};
 
 	BOWSER {
-		regex: r"(?i-u)^bowser$",
+        external: External::BOWSER,
 		short: "Bowser",
 		long: "Bowser",
 		traction: 0.06,
 	};
 
 	LINK {
-		regex: r"(?i-u)^link$",
+        external: External::LINK,
 		short: "Link",
 		long: "Link",
 		traction: 0.1,
 	};
 
 	LUIGI {
-		regex: r"(?i-u)^luigi$",
+        external: External::LUIGI,
 		short: "Luigi",
 		long: "Luigi",
 		traction: 0.025,
 	};
 
 	MARIO {
-		regex: r"(?i-u)^mario$",
+        external: External::MARIO,
 		short: "Mario",
 		long: "Mario",
 		traction: 0.06,
 	};
 
 	MARTH {
-		regex: r"(?i-u)^marth$",
+        external: External::MARTH,
 		short: "Marth",
 		long: "Marth",
 		traction: 0.06,
 	};
 
 	MEWTWO {
-		regex: r"(?i-u)^mewtwo|mew2|m2$",
+        external: External::MEWTWO,
 		short: "Mewtwo",
 		long: "Mewtwo",
 		traction: 0.04,
 	};
 
 	NESS {
-		regex: r"(?i-u)^ness$",
+        external: External::NESS,
 		short: "Ness",
 		long: "Ness",
 		traction: 0.06,
 	};
 
 	PEACH {
-		regex: r"(?i-u)^peach$",
+        external: External::PEACH,
 		short: "Peach",
 		long: "Peach",
 		traction: 0.1,
 	};
 
 	PIKACHU {
-		regex: r"(?i-u)^pika(chu)?$",
+        external: External::PIKACHU,
 		short: "Pika",
 		long: "Pikachu",
 		traction: 0.09,
 	};
 
 	ICE_CLIMBERS {
-		regex: r"(?i-u)^ic(|s|ies|e[ _]?climbers)$",
+        external: External::ICE_CLIMBERS,
 		short: "ICs",
 		long: "Ice Climbers",
 		traction: 0.035,
 	};
 
 	JIGGLYPUFF {
-		regex: r"(?i-u)^(jiggly)?puff|jiggs$",
+        external: External::JIGGLYPUFF,
 		short: "Puff",
 		long: "Jigglypuff",
 		traction: 0.09,
 	};
 
 	SAMUS {
-		regex: r"(?i-u)^samus$",
+        external: External::SAMUS,
 		short: "Samus",
 		long: "Samus",
 		traction: 0.06,
 	};
 
 	YOSHI {
-		regex: r"(?i-u)^yoshi$",
+        external: External::YOSHI,
 		short: "Yoshi",
 		long: "Yoshi",
 		traction: 0.06,
 	};
 
 	ZELDA {
-		regex: r"(?i-u)^zelda$",
+        external: External::ZELDA,
 		short: "Zelda",
 		long: "Zelda",
 		traction: 0.1,
 	};
 
 	SHEIK {
-		regex: r"(?i-u)^sheik$",
+        external: External::SHEIK,
 		short: "Sheik",
 		long: "Sheik",
 		traction: 0.08,
 	};
 
 	FALCO {
-		regex: r"(?i-u)^falco$",
+        external: External::FALCO,
 		short: "Falco",
 		long: "Falco",
 		traction: 0.08,
 	};
 
 	YOUNG_LINK {
-		regex: r"(?i-u)^(y\.?|young)[ _]?(l|link$",
+        external: External::YOUNG_LINK,
 		short: "YL",
 		long: "Young Link",
 		traction: 0.08,
 	};
 
 	DR_MARIO {
-		regex: r"(?i-u)^doc|dr\.?[ _]?mario$",
+        external: External::DR_MARIO,
 		short: "Doc",
 		long: "Dr. Mario",
 		traction: 0.06,
 	};
 
 	ROY {
-		regex: r"(?i-u)^roy$",
+        external: External::ROY,
 		short: "Roy",
 		long: "Roy",
 		traction: 0.06,
 	};
 
 	PICHU {
-		regex: r"(?i-u)^pichu$",
+        external: External::PICHU,
 		short: "Pichu",
 		long: "Pichu",
 		traction: 0.1,
 	};
 
 	GANONDORF {
-		regex: r"(?i-u)^ganon(dorf)?$",
+        external: External::GANONDORF,
 		short: "Ganon",
 		long: "Ganondorf",
 		traction: 0.07,
 	};
 
 	MASTER_HAND {
-		regex: r"(?i-u)^master[ _]?hand$",
+        external: External::MASTER_HAND,
 		short: "Master Hand",
 		long: "Master Hand",
 		traction: 0.0,
 	};
 
 	WIRE_FRAME_MALE {
-		regex: r"(?i-u)^male[ _]?wire[ _]?frame|wire[ _]?frame[ _]?male$",
+        external: External::WIRE_FRAME_MALE,
 		short: "Male Wireframe",
 		long: "Male Wireframe",
 		traction: 0.0,
 	};
 
 	WIRE_FRAME_FEMALE {
-		regex: r"(?i-u)^female[ _]?wire[ _]?frame|wire[ _]?frame[ _]?female$",
+        external: External::WIRE_FRAME_FEMALE,
 		short: "Female Wireframe",
 		long: "Female Wireframe",
 		traction: 0.0,
 	};
 
 	GIGA_BOWSER {
-		regex: r"(?i-u)^giga[ _]?bowser$",
+        external: External::GIGA_BOWSER,
 		short: "Giga Bowser",
 		long: "Giga Bowser",
 		traction: 0.0,
 	};
 
 	CRAZY_HAND {
-		regex: r"(?i-u)^crazy[ _]?hand$",
+        external: External::CRAZY_HAND,
 		short: "Crazy Hand",
 		long: "Crazy Hand",
 		traction: 0.0,
 	};
 
 	SANDBAG {
-		regex: r"(?i-u)^sandbag$",
+        external: External::SANDBAG,
 		short: "Sandbag",
 		long: "Sandbag",
 		traction: 0.0,
 	};
 
 	POPO {
-		regex: r"(?i-u)^popo$",
+        external: External::POPO,
 		short: "Popo",
 		long: "Popo",
 		traction: 0.035,
 	};
+});
+
+info_regex!(Info {
+    CAPTAIN_FALCON: r"(?i-u)^(capt(ain|\.)?[ _]?)?falcon$",
+    DONKEY_KONG: r"(?i-u)^dk|(donkey[ _]?kong$",
+    FOX: r"(?i-u)^fox$",
+    GAME_AND_WATCH: r"(?i-u)^(g|game)[ _]?(and|&|n)[ _]?(w|watch)$",
+    KIRBY: r"(?i-u)^kirby$",
+    BOWSER: r"(?i-u)^bowser$",
+    LINK: r"(?i-u)^link$",
+    LUIGI: r"(?i-u)^luigi$",
+    MARIO: r"(?i-u)^mario$",
+    MARTH: r"(?i-u)^marth$",
+    MEWTWO: r"(?i-u)^mewtwo|mew2|m2$",
+    NESS: r"(?i-u)^ness$",
+    PEACH: r"(?i-u)^peach$",
+    PIKACHU: r"(?i-u)^pika(chu)?$",
+    ICE_CLIMBERS: r"(?i-u)^ic(|s|ies|e[ _]?climbers)$",
+    JIGGLYPUFF: r"(?i-u)^(jiggly)?puff|jiggs$",
+    SAMUS: r"(?i-u)^samus$",
+    YOSHI: r"(?i-u)^yoshi$",
+    ZELDA: r"(?i-u)^zelda$",
+    SHEIK: r"(?i-u)^sheik$",
+    FALCO: r"(?i-u)^falco$",
+    YOUNG_LINK: r"(?i-u)^(y\.?|young)[ _]?(l|link$",
+    DR_MARIO: r"(?i-u)^doc|dr\.?[ _]?mario$",
+    ROY: r"(?i-u)^roy$",
+    PICHU: r"(?i-u)^pichu$",
+    GANONDORF: r"(?i-u)^ganon(dorf)?$",
+    MASTER_HAND: r"(?i-u)^master[ _]?hand$",
+    WIRE_FRAME_MALE: r"(?i-u)^male[ _]?wire[ _]?frame|wire[ _]?frame[ _]?male$",
+    WIRE_FRAME_FEMALE: r"(?i-u)^female[ _]?wire[ _]?frame|wire[ _]?frame[ _]?female$",
+    GIGA_BOWSER: r"(?i-u)^giga[ _]?bowser$",
+    CRAZY_HAND: r"(?i-u)^crazy[ _]?hand$",
+    SANDBAG: r"(?i-u)^sandbag$",
+    POPO: r"(?i-u)^popo$",
 });
