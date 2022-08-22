854befa52bedb6ceaeb53233d78b3b0c6172efde
Nick Condron <ncondron42@gmail.com>
2022-08-22 04:00:11 -0400

    remove indexing fields

--- peppi/src/info/character.rs
+++ peppi/src/info/character.rs
@@ -1,222 +1,186 @@
-pub mod external {
+mod external {
     use crate::model::enums::character::External;
 
     #[non_exhaustive]
     #[derive(Clone, Debug)]
     pub struct Info {
-        pub external: External,
         pub short_name: &'static str,
         pub long_name: &'static str,
     }
 
     info!(External => Info {
         CAPTAIN_FALCON {
-            external: External::CAPTAIN_FALCON,
             short_name: "Falcon",
             long_name: "Captain Falcon",
         };
 
         DONKEY_KONG {
-            external: External::DONKEY_KONG,
             short_name: "DK",
             long_name: "Donkey Kong",
         };
 
         FOX {
-            external: External::FOX,
             short_name: "Fox",
             long_name: "Fox",
         };
 
         GAME_AND_WATCH {
-            external: External::GAME_AND_WATCH,
             short_name: "G&W",
             long_name: "Game and Watch",
         };
 
         KIRBY {
-            external: External::KIRBY,
             short_name: "Kirby",
             long_name: "Kirby",
         };
 
         BOWSER {
-            external: External::BOWSER,
             short_name: "Bowser",
             long_name: "Bowser",
         };
 
         LINK {
-            external: External::LINK,
             short_name: "Link",
             long_name: "Link",
         };
 
         LUIGI {
-            external: External::LUIGI,
             short_name: "Luigi",
             long_name: "Luigi",
         };
 
         MARIO {
-            external: External::MARIO,
             short_name: "Mario",
             long_name: "Mario",
         };
 
         MARTH {
-            external: External::MARTH,
             short_name: "Marth",
             long_name: "Marth",
         };
 
         MEWTWO {
-            external: External::MEWTWO,
             short_name: "Mewtwo",
             long_name: "Mewtwo",
         };
 
         NESS {
-            external: External::NESS,
             short_name: "Ness",
             long_name: "Ness",
         };
 
         PEACH {
-            external: External::PEACH,
             short_name: "Peach",
             long_name: "Peach",
         };
 
         PIKACHU {
-            external: External::PIKACHU,
             short_name: "Pika",
             long_name: "Pikachu",
         };
 
         ICE_CLIMBERS {
-            external: External::ICE_CLIMBERS,
             short_name: "ICs",
             long_name: "Ice Climbers",
         };
 
         JIGGLYPUFF {
-            external: External::JIGGLYPUFF,
             short_name: "Puff",
             long_name: "Jigglypuff",
         };
 
         SAMUS {
-            external: External::SAMUS,
             short_name: "Samus",
             long_name: "Samus",
         };
 
         YOSHI {
-            external: External::YOSHI,
             short_name: "Yoshi",
             long_name: "Yoshi",
         };
 
         ZELDA {
-            external: External::ZELDA,
             short_name: "Zelda",
             long_name: "Zelda",
         };
 
         SHEIK {
-            external: External::SHEIK,
             short_name: "Sheik",
             long_name: "Sheik",
         };
 
         FALCO {
-            external: External::FALCO,
             short_name: "Falco",
             long_name: "Falco",
         };
 
         YOUNG_LINK {
-            external: External::YOUNG_LINK,
             short_name: "YL",
             long_name: "Young Link",
         };
 
         DR_MARIO {
-            external: External::DR_MARIO,
             short_name: "Doc",
             long_name: "Dr. Mario",
         };
 
         ROY {
-            external: External::ROY,
             short_name: "Roy",
             long_name: "Roy",
         };
 
         PICHU {
-            external: External::PICHU,
             short_name: "Pichu",
             long_name: "Pichu",
         };
 
         GANONDORF {
-            external: External::GANONDORF,
             short_name: "Ganon",
             long_name: "Ganondorf",
         };
 
         MASTER_HAND {
-            external: External::MASTER_HAND,
             short_name: "Master Hand",
             long_name: "Master Hand",
         };
 
         WIRE_FRAME_MALE {
-            external: External::WIRE_FRAME_MALE,
             short_name: "Male Wireframe",
             long_name: "Male Wireframe",
         };
 
         WIRE_FRAME_FEMALE {
-            external: External::WIRE_FRAME_FEMALE,
             short_name: "Female Wireframe",
             long_name: "Female Wireframe",
         };
 
         GIGA_BOWSER {
-            external: External::GIGA_BOWSER,
             short_name: "Giga Bowser",
             long_name: "Giga Bowser",
         };
 
         CRAZY_HAND {
-            external: External::CRAZY_HAND,
             short_name: "Crazy Hand",
             long_name: "Crazy Hand",
         };
 
         SANDBAG {
-            external: External::SANDBAG,
             short_name: "Sandbag",
             long_name: "Sandbag",
         };
 
         POPO {
-            external: External::POPO,
             short_name: "Popo",
             long_name: "Popo",
         };
     });
 }
 
-pub mod internal {
+mod internal {
     use crate::model::enums::character::Internal;
-
     #[non_exhaustive]
     #[derive(Clone, Debug)]
     pub struct Info {
-        pub internal: Internal,
         pub jumpsquat: u8,
         pub empty_landing_lag: u8,
         pub can_walljump: bool,
@@ -224,210 +188,180 @@ pub mod internal {
 
     info!(Internal => Info {
         MARIO {
-            internal: Internal::MARIO,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         FOX {
-            internal: Internal::FOX,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         CAPTAIN_FALCON {
-            internal: Internal::CAPTAIN_FALCON,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         DONKEY_KONG {
-            internal: Internal::DONKEY_KONG,
             jumpsquat: 5,
             empty_landing_lag: 5,
             can_walljump: false,
         };
 
         KIRBY {
-            internal: Internal::KIRBY,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         BOWSER {
-            internal: Internal::BOWSER,
             jumpsquat: 8,
             empty_landing_lag: 6,
             can_walljump: false,
         };
 
         LINK {
-            internal: Internal::LINK,
             jumpsquat: 6,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         SHEIK {
-            internal: Internal::SHEIK,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         NESS {
-            internal: Internal::NESS,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         PEACH {
-            internal: Internal::PEACH,
             jumpsquat: 5,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         POPO {
-            internal: Internal::POPO,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         NANA {
-            internal: Internal::NANA,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         PIKACHU {
-            internal: Internal::PIKACHU,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         SAMUS {
-            internal: Internal::SAMUS,
             jumpsquat: 3,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         YOSHI {
-            internal: Internal::YOSHI,
             jumpsquat: 5,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         JIGGLYPUFF {
-            internal: Internal::JIGGLYPUFF,
             jumpsquat: 5,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         MEWTWO {
-            internal: Internal::MEWTWO,
             jumpsquat: 5,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         LUIGI {
-            internal: Internal::LUIGI,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         MARTH {
-            internal: Internal::MARTH,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         ZELDA {
-            internal: Internal::ZELDA,
             jumpsquat: 6,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         YOUNG_LINK {
-            internal: Internal::YOUNG_LINK,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         DR_MARIO {
-            internal: Internal::DR_MARIO,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         FALCO {
-            internal: Internal::FALCO,
             jumpsquat: 5,
             empty_landing_lag: 4,
             can_walljump: true,
         };
 
         PICHU {
-            internal: Internal::PICHU,
             jumpsquat: 3,
             empty_landing_lag: 2,
             can_walljump: true,
         };
 
         GAME_AND_WATCH {
-            internal: Internal::GAME_AND_WATCH,
             jumpsquat: 4,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         GANONDORF {
-            internal: Internal::GANONDORF,
             jumpsquat: 6,
             empty_landing_lag: 5,
             can_walljump: false,
         };
 
         ROY {
-            internal: Internal::ROY,
             jumpsquat: 5,
             empty_landing_lag: 4,
             can_walljump: false,
         };
 
         WIRE_FRAME_MALE {
-            internal: Internal::WIRE_FRAME_MALE,
             jumpsquat: 7,
             empty_landing_lag: 15,
             can_walljump: false,
         };
 
         WIRE_FRAME_FEMALE {
-            internal: Internal::WIRE_FRAME_FEMALE,
             jumpsquat: 7,
             empty_landing_lag: 15,
             can_walljump: false,
         };
 
         GIGA_BOWSER {
-            internal: Internal::GIGA_BOWSER,
             jumpsquat: 6,
             empty_landing_lag: 30,
             can_walljump: false,
--- peppi/src/info/stage.rs
+++ peppi/src/info/stage.rs
@@ -3,146 +3,122 @@ use crate::model::enums::stage::Stage;
 #[non_exhaustive]
 #[derive(Clone, Debug)]
 pub struct Info {
-    pub stage: Stage,
     pub short_name: &'static str,
     pub long_name: &'static str,
 }
 
 info!(Stage => Info {
 	FOUNTAIN_OF_DREAMS {
-        stage: Stage::FOUNTAIN_OF_DREAMS,
         short_name: "FoD",
         long_name: "Fountain of Dreams",
     };
 
 	POKEMON_STADIUM {
-        stage: Stage::POKEMON_STADIUM,
         short_name: "PS",
         long_name: "Pokemon Stadium",
     };
 
 	PRINCESS_PEACHS_CASTLE {
-        stage: Stage::PRINCESS_PEACHS_CASTLE,
         short_name: "PPC",
         long_name: "Princess Peach's Castle",
     };
 
 	KONGO_JUNGLE {
-        stage: Stage::KONGO_JUNGLE,
         short_name: "KJ",
         long_name: "Kongo Jungle",
     };
 
 	BRINSTAR {
-        stage: Stage::BRINSTAR,
         short_name: "Brinstar",
         long_name: "Brinstar",
     };
 
 	CORNERIA {
-        stage: Stage::CORNERIA,
         short_name: "Corneria",
         long_name: "Corneria",
     };
 
 	YOSHIS_STORY {
-        stage: Stage::YOSHIS_STORY,
         short_name: "YS",
         long_name: "Yoshi's Story",
     };
 
 	ONETT {
-        stage: Stage::ONETT,
         short_name: "Onett",
         long_name: "Onett",
     };
 
 	MUTE_CITY {
-        stage: Stage::MUTE_CITY,
         short_name: "MC",
         long_name: "Mute City",
     };
 
 	RAINBOW_CRUISE {
-        stage: Stage::RAINBOW_CRUISE,
         short_name: "RC",
         long_name: "Rainbow Cruise",
     };
 
 	JUNGLE_JAPES {
-        stage: Stage::JUNGLE_JAPES,
         short_name: "JJ",
         long_name: "Jungle Japes",
     };
 
 	GREAT_BAY {
-        stage: Stage::GREAT_BAY,
         short_name: "GB",
         long_name: "Great Bay",
     };
 
 	HYRULE_TEMPLE {
-        stage: Stage::HYRULE_TEMPLE,
         short_name: "HT",
         long_name: "Hyrule Temple",
     };
 
 	BRINSTAR_DEPTHS {
-        stage: Stage::BRINSTAR_DEPTHS,
         short_name: "BD",
         long_name: "Brinstar Depths",
     };
 
 	YOSHIS_ISLAND {
-        stage: Stage::YOSHIS_ISLAND,
         short_name: "YI",
         long_name: "Yoshi's Island",
     };
 
 	GREEN_GREENS {
-        stage: Stage::GREEN_GREENS,
         short_name: "GG",
         long_name: "Green Greens",
     };
 
 	FOURSIDE {
-        stage: Stage::FOURSIDE,
         short_name: "Fourside",
         long_name: "Fourside",
     };
 
 	MUSHROOM_KINGDOM_I {
-        stage: Stage::MUSHROOM_KINGDOM_I,
         short_name: "MKI",
         long_name: "Mushroom Kingdom I",
     };
 
 	MUSHROOM_KINGDOM_II {
-        stage: Stage::MUSHROOM_KINGDOM_II,
         short_name: "MKII",
         long_name: "Mushroom Kingdom II",
     };
 
 	VENOM {
-        stage: Stage::VENOM,
         short_name: "Venom",
         long_name: "Venom",
     };
 
 	POKE_FLOATS {
-        stage: Stage::POKE_FLOATS,
         short_name: "PF",
         long_name: "Poke Floats",
     };
 
 	BIG_BLUE {
-        stage: Stage::BIG_BLUE,
         short_name: "BB",
         long_name: "Big Blue",
     };
 
 	ICICLE_MOUNTAIN {
-        stage: Stage::ICICLE_MOUNTAIN,
         short_name: "IM",
         long_name: "Icicle Mountain",
     };
@@ -150,37 +126,31 @@ info!(Stage => Info {
     // No ICETOP (unplayable in melee without hacks)
 
 	FLAT_ZONE {
-        stage: Stage::FLAT_ZONE,
         short_name: "FZ",
         long_name: "Flat Zone",
     };
 
 	DREAM_LAND_N64 {
-        stage: Stage::DREAM_LAND_N64,
         short_name: "DL64",
         long_name: "Dream Land N64",
     };
 
 	YOSHIS_ISLAND_N64 {
-        stage: Stage::YOSHIS_ISLAND_N64,
         short_name: "YI64",
         long_name: "Yoshi's Island N64",
     };
 
 	KONGO_JUNGLE_N64 {
-        stage: Stage::KONGO_JUNGLE_N64,
         short_name: "KJ64",
         long_name: "Kongo Jungle N64",
     };
 
 	BATTLEFIELD {
-        stage: Stage::BATTLEFIELD,
         short_name: "BF",
         long_name: "Battlefield",
     };
 
 	FINAL_DESTINATION {
-        stage: Stage::FINAL_DESTINATION,
         short_name: "FD",
         long_name: "Final Destination",
     };
