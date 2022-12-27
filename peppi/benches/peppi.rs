c01877a75480eacef13a26ced79d01a3bc531ef5
Nick Condron <ncondron42@gmail.com>
2022-12-27 00:04:35 -0500

    Add Criterion benchmarks

---
+++ peppi/benches/peppi.rs
@@ -0,0 +1,63 @@
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
+use peppi::{self, serde::de};
+
+use std::{fs, path::PathBuf};
+
+struct DumbHandler;
+impl de::Handlers for DumbHandler {}
+
+pub fn bench(c: &mut Criterion) {
+	let mut group = c.benchmark_group("replays");
+	let dir = PathBuf::from("benches/data");
+	for replay in fs::read_dir(dir).unwrap() {
+		let path = replay.unwrap().path();
+		let name = path.file_name().unwrap().to_str().unwrap().to_string();
+		let contents = fs::read(path).unwrap();
+
+		group.throughput(Throughput::Bytes(contents.len().try_into().unwrap()));
+		group.bench_with_input(
+			BenchmarkId::new("into_game", &name),
+			&contents,
+			|b, contents| {
+				b.iter_batched(
+					|| contents.as_slice(),
+					|mut buf| peppi::game(&mut buf, None, None),
+					BatchSize::LargeInput,
+				)
+			},
+		);
+		group.bench_with_input(
+			BenchmarkId::new("event_handlers", &name),
+			&contents,
+			|b, contents| {
+				b.iter_batched(
+					|| contents.as_slice(),
+					|mut buf| peppi::parse(&mut buf, &mut DumbHandler, None),
+					BatchSize::SmallInput,
+				)
+			},
+		);
+		group.bench_with_input(
+			BenchmarkId::new("skip_frames", &name),
+			&contents,
+			|b, contents| {
+				b.iter_batched(
+					|| contents.as_slice(),
+					|mut buf| {
+						let opts = de::Opts {
+							skip_frames: true,
+							debug_dir: None,
+						};
+						peppi::game(&mut buf, Some(&opts), None)
+					},
+					BatchSize::SmallInput,
+				)
+			},
+		);
+	}
+
+	group.finish();
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
