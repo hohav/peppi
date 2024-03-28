e8f4a2f60dbd69f3eaee7cb54fb6e53ed6b7cad9
melkor <hohav@fastmail.com>
2024-03-28 13:04:41 -0700

    Refactor

--- src/io/slippi/ser.rs
+++ src/io/slippi/ser.rs
@@ -13,29 +13,70 @@ use crate::{
 
 type BE = byteorder::BigEndian;
 
-fn payload_sizes(game: &Game) -> Vec<(u8, u16)> {
-	// The order of the sizes list is important for round-tripping,
-	// which is why we use a Vec rather than a HashMap.
-	let mut sizes = Vec::new();
+struct PayloadSizes {
+	/// Order matters for round-tripping, hence Vec rather than HashMap.
+	sizes: Vec<(u8, u16)>,
+}
+
+impl PayloadSizes {
+	fn new() -> Self {
+		Self { sizes: Vec::new() }
+	}
+
+	fn push(&mut self, event: Event, size: usize) {
+		self.sizes.push((event as u8, size.try_into().unwrap()))
+	}
+
+	fn raw_size(&self, game: &Game) -> u32 {
+		use Event::*;
+
+		let counts = frame_counts(&game.frames);
+		let sizes: std::collections::HashMap<u8, u16> =
+			self.sizes.iter().map(|(k, v)| (*k, *v)).collect();
+		1 + 1 + (3 * self.sizes.len() as u32) // Payload sizes
+			+ 1 + sizes[&(GameStart as u8)] as u32 // GameStart
+			+ 1 + sizes[&(GameEnd as u8)] as u32 // GameEnd
+			+ match game.quirks.map_or(false, |q| q.double_game_end) {
+				true => 1 + sizes[&(GameEnd as u8)] as u32, // ...and another for good measure
+				_ => 0u32,
+			}
+			+ counts.frame_data * (1 + sizes[&(FramePre as u8)] as u32) // FramePre
+			+ counts.frame_data * (1 + sizes[&(FramePost as u8)] as u32) // FramePost
+			+ sizes.get(&(FrameStart as u8)).map_or(0, |s| counts.frames * (1 + *s as u32)) // FrameStart
+			+ sizes.get(&(FrameEnd as u8)).map_or(0, |s| counts.frames * (1 + *s as u32)) // FrameEnd
+			+ sizes.get(&(Item as u8)).map_or(0, |s| counts.items * (1 + *s as u32)) // Item
+			+ game.gecko_codes.as_ref().map_or(0, gecko_codes_size)
+	}
+}
+
+fn payload_sizes(game: &Game) -> PayloadSizes {
+	let mut sizes = PayloadSizes::new();
 	let ver = game.start.slippi.version.clone();
 
-	// +4 bytes for frame number, or +6 for frame number / port / follower
-	sizes.push((Event::GameStart as u8, game.start.bytes.0.len() as u16));
-	sizes.push((Event::FramePre as u8, 6 + Pre::size(ver) as u16));
-	sizes.push((Event::FramePost as u8, 6 + Post::size(ver) as u16));
-	sizes.push((Event::GameEnd as u8, game.end.as_ref().map_or(game::End::size(ver), |e| e.bytes.0.len()) as u16));
+	const FRAME_NUMBER: usize = std::mem::size_of::<i32>();
+	const PORT: usize = 2 * std::mem::size_of::<u8>(); // port number + is_follower
+
+	sizes.push(Event::GameStart, game.start.bytes.0.len());
+	sizes.push(Event::FramePre, FRAME_NUMBER + PORT + Pre::size(ver));
+	sizes.push(Event::FramePost, FRAME_NUMBER + PORT + Post::size(ver));
+	sizes.push(
+		Event::GameEnd,
+		game.end
+			.as_ref()
+			.map_or(game::End::size(ver), |e| e.bytes.0.len()),
+	);
 
 	if ver.gte(2, 2) {
-		sizes.push((Event::FrameStart as u8, 4 + Start::size(ver) as u16));
+		sizes.push(Event::FrameStart, FRAME_NUMBER + Start::size(ver));
 		if ver.gte(3, 0) {
-			sizes.push((Event::Item as u8, 4 + Item::size(ver) as u16));
+			sizes.push(Event::Item, FRAME_NUMBER + Item::size(ver));
 			if ver.gte(3, 0) {
-				sizes.push((Event::FrameEnd as u8, 4 + End::size(ver) as u16));
+				sizes.push(Event::FrameEnd, FRAME_NUMBER + End::size(ver));
 				if ver.gte(3, 3) {
 					if let Some(codes) = &game.gecko_codes {
-						// higher-order bits of actual_size are lost, matching Slippi's behavior
-						sizes.push((Event::GeckoCodes as u8, codes.actual_size as u16));
-						sizes.push((Event::MessageSplitter as u8, 516));
+						// discard higher-order bits of actual_size, matching Slippi's behavior
+						sizes.push(Event::GeckoCodes, codes.actual_size as u16 as usize);
+						sizes.push(Event::MessageSplitter, 516);
 					}
 				}
 			}
@@ -103,41 +144,20 @@ fn gecko_codes_size(gecko_codes: &GeckoCodes) -> u32 {
 	num_blocks * (512 + 5)
 }
 
-fn raw_size(game: &Game, payload_sizes: &Vec<(u8, u16)>) -> u32 {
-	use Event::*;
-
-	let counts = frame_counts(&game.frames);
-	let sizes: std::collections::HashMap<u8, u16> =
-		payload_sizes.iter().map(|(k, v)| (*k, *v)).collect();
-	1 + 1 + (3 * payload_sizes.len() as u32) // Payload sizes
-		+ 1 + sizes[&(GameStart as u8)] as u32 // GameStart
-		+ 1 + sizes[&(GameEnd as u8)] as u32 // GameEnd
-		+ match game.quirks.map_or(false, |q| q.double_game_end) {
-			true => 1 + sizes[&(GameEnd as u8)] as u32, // ...and another for good measure
-			_ => 0u32,
-		}
-		+ counts.frame_data * (1 + sizes[&(FramePre as u8)] as u32) // FramePre
-		+ counts.frame_data * (1 + sizes[&(FramePost as u8)] as u32) // FramePost
-		+ sizes.get(&(FrameStart as u8)).map_or(0, |s| counts.frames * (1 + *s as u32)) // FrameStart
-		+ sizes.get(&(FrameEnd as u8)).map_or(0, |s| counts.frames * (1 + *s as u32)) // FrameEnd
-		+ sizes.get(&(Item as u8)).map_or(0, |s| counts.items * (1 + *s as u32)) // Item
-		+ game.gecko_codes.as_ref().map_or(0, gecko_codes_size)
-}
-
 /// Writes a Slippi-format game to `w`.
 /// Returns an error if the game's version is higher than `MAX_SUPPORTED_VERSION`.
 pub fn write<W: Write>(w: &mut W, game: &Game) -> Result<()> {
 	slippi::assert_max_version(game.start.slippi.version)?;
 
 	let payload_sizes = payload_sizes(game);
-	let raw_size = raw_size(game, &payload_sizes);
 
 	w.write_all(&slippi::FILE_SIGNATURE)?;
-	w.write_u32::<BE>(raw_size)?;
+	w.write_u32::<BE>(payload_sizes.raw_size(game))?;
 
 	w.write_u8(Event::Payloads as u8)?;
-	w.write_u8((payload_sizes.len() * 3 + 1).try_into().unwrap())?; // see note in `parse::payload_sizes`
-	for (event, size) in payload_sizes {
+	// see "off-by-one" note in `de::parse_payloads`
+	w.write_u8((payload_sizes.sizes.len() * 3 + 1).try_into().unwrap())?;
+	for (event, size) in payload_sizes.sizes {
 		w.write_u8(event)?;
 		w.write_u16::<BE>(size)?;
 	}
