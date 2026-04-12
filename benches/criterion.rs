8201b6b438c984cee44c8782a926587e2b58f81b
melkor <hohav@fastmail.com>
2026-04-12 10:35:15 -0700

    Benchmark `into_struct_array`

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -1,6 +1,7 @@
 use criterion::{BatchSize, BenchmarkId, Criterion, criterion_group, criterion_main};
 use peppi::{
 	self,
+	game::port_occupancy,
 	io::slippi::de::{Opts, read},
 };
 
@@ -18,7 +19,30 @@ pub fn into_game(c: &mut Criterion) {
 			|b, contents| {
 				b.iter_batched(
 					|| contents.as_slice(),
-					|buf| read(&mut Cursor::new(buf), None),
+					|buf| read(&mut Cursor::new(buf), None).unwrap(),
+					BatchSize::LargeInput,
+				)
+			},
+		);
+	}
+}
+
+pub fn into_struct_array(c: &mut Criterion) {
+	let dir = PathBuf::from("benches/data");
+	for replay in fs::read_dir(dir).unwrap() {
+		let path = replay.unwrap().path();
+		let name = path.file_name().unwrap().to_str().unwrap().to_string();
+		let contents = fs::read(path).unwrap();
+		c.bench_with_input(
+			BenchmarkId::new("into_struct_array", &name),
+			&contents,
+			|b, contents| {
+				b.iter_batched(
+					|| contents.as_slice(),
+					|buf| {
+						let game = read(&mut Cursor::new(buf), None).unwrap();
+						game.frames.into_struct_array(game.start.slippi.version, &port_occupancy(&game.start))
+					},
 					BatchSize::LargeInput,
 				)
 			},
@@ -62,10 +86,17 @@ criterion_group! {
 }
 
 criterion_group! {
+	name = bench_into_struct_array;
+	config = Criterion::default()
+		.warm_up_time(Duration::from_secs(1));
+	targets = into_struct_array
+}
+
+criterion_group! {
 	name = bench_skip_frames;
 	config = Criterion::default()
 		.warm_up_time(Duration::from_secs(1));
 	targets = skip_frames
 }
 
-criterion_main!(bench_into_game, bench_skip_frames);
+criterion_main!(bench_into_game, bench_into_struct_array, bench_skip_frames);
